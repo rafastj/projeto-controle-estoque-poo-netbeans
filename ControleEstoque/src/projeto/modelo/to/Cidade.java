1dbb711c9aef1389f88041ff1d2255bd31d5aea0
danielvdantas <danielvdantas@hotmail.com>
2012-05-22 18:12:17 +0000

    Organização dos códigos!

--- ControleEstoque/src/projeto/modelo/to/Cidade.java
+++ ControleEstoque/src/projeto/modelo/to/Cidade.java
@@ -9,37 +9,37 @@ package projeto.modelo.to;
  */
 public class Cidade {
 	
-	private int cidades_Codigo;
-	private String cidades_Nome;
-	
-	public Cidade () {}
-	
-	public Cidade (String cidades_Nome) {
-		this.cidades_Nome = cidades_Nome;
-	}
+    private int cidades_Codigo;
+    private String cidades_Nome;
 
-	public int getCidades_Codigo() {
-		return cidades_Codigo;
-	}
+    public Cidade () {}
 
-	public void setCidades_Codigo(int cidades_Codigo) {
-		this.cidades_Codigo = cidades_Codigo;
-	}
+    public Cidade (String cidades_Nome) {
+            this.cidades_Nome = cidades_Nome;
+    }
 
-	public String getCidades_Nome() {
-		return cidades_Nome;
-	}
+    public int getCidades_Codigo() {
+            return cidades_Codigo;
+    }
 
-	public void setCidades_Nome(String cidades_Nome) {
-		this.cidades_Nome = cidades_Nome;
-	}
-	
-	/**
+    public void setCidades_Codigo(int cidades_Codigo) {
+            this.cidades_Codigo = cidades_Codigo;
+    }
+
+    public String getCidades_Nome() {
+            return cidades_Nome;
+    }
+
+    public void setCidades_Nome(String cidades_Nome) {
+            this.cidades_Nome = cidades_Nome;
+    }
+
+    /**
      * Verifica se todos os campos obrigatórios estão OK.
      * @return true Se não houver problemas.
      */
-	public boolean isValido(){
-        if ( (getCidades_Nome().equals("")) || (getCidades_Nome()).equals(null) || (getCidades_Codigo() == 0) ){
+    public boolean isValido(){
+        if ( (getCidades_Nome().equals("")) || (getCidades_Nome() == null) || (getCidades_Codigo() == 0) ){
             return false;
         }
         return true;
@@ -50,7 +50,7 @@ public class Cidade {
      * @return null, ou o nome do primeiro campo inválido.
      */
     public String getValidoString(){
-        if ( (getCidades_Nome().equals("")) || (getCidades_Nome()).equals(null) ){
+        if ( (getCidades_Nome().equals("")) || (getCidades_Nome()) == null ){
             return "cidades_Nome";
         }
         return null;
