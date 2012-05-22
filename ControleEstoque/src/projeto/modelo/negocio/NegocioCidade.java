23e10e6a45bbd2367e344dc4db2a21cb7739db60
danielvdantas <danielvdantas@hotmail.com>
2012-05-22 17:11:08 +0000

    Atualização nas classes NegocioCidade, NegocioEndereco, NegocioFornecedor.

--- ControleEstoque/src/projeto/modelo/negocio/NegocioCidade.java
+++ ControleEstoque/src/projeto/modelo/negocio/NegocioCidade.java
@@ -13,7 +13,7 @@ import projeto.modelo.repositorio.RepositorioCidade;
 import projeto.modelo.to.Cidade;
 
 /**
- * @author DANIEL VALENï¿½A
+ * @author DANIEL VALENÇA
  *
  */
 public class NegocioCidade {
@@ -22,7 +22,7 @@ public class NegocioCidade {
 	
 	/**
 	 * Construtor inicializa o atributo "rep",
-     * responsï¿½vel pela camada de Acesso a Dados;
+     * responsável pela camada de Acesso a Dados;
 	 */
 	public NegocioCidade() {
 		rep = new RepositorioCidade();
@@ -35,37 +35,37 @@ public class NegocioCidade {
 	 */
 	public void salvar(Cidade cd) throws GeralException {
 		
-		if( (cd.getCidades_Nome() == null) || (cd.getCidades_Nome().equals("")) ) {
-			throw new GeralException("Digite o nome ou o cï¿½dido da cidade!");
-		}
-		
-		/*validaï¿½ï¿½o dos campos pelo objeto e retorna o campo com problema*/
-        String campo = cd.getValidoString();
-        if( campo != null ){
-            throw new GeralException(campo + " invï¿½lido");
-        }
+            if( (cd.getCidades_Nome() == null) || (cd.getCidades_Nome().equals("")) ) {
+                    throw new GeralException("Digite o nome ou o códido da cidade!");
+            }
 
-        /*validaï¿½ï¿½o dos campos pelo objeto e retorna true/false */
-        if( cd.isValido() ){
-            throw new GeralException("invï¿½lido");
-        }
+            /*validação dos campos pelo objeto e retorna o campo com problema*/
+            String campo = cd.getValidoString();
+            if( campo != null ){
+                throw new GeralException(campo + " inválido");
+            }
+
+            /*validação dos campos pelo objeto e retorna true/false */
+            if( cd.isValido() ){
+                throw new GeralException("inválido");
+            }
 		
 		try {
 			Cidade cdConsult = rep.consultar(cd.getCidades_Nome());
 			if( cdConsult != null ){
-				throw new GeralException("Cidade jï¿½ estï¿½ cadastrada!");
+				throw new GeralException("Cidade já está cadastrada!");
 			}
 			rep.salvar(cd);
 			throw new GeralException("Registro foi salvo com sucesso!");
-		} catch(RepositorioException e) {
-			throw new GeralException("Erro de programaï¿½ï¿½o!");
-		} catch(ConexaoException e) {
-			throw new GeralException("Banco de dados inacessï¿½vel!!!");
-		}
+		}catch(RepositorioException e){
+                    throw new GeralException("Erro de programação!");
+                }catch(ConexaoException e){
+                    throw new GeralException("O banco de dados não está acessível no momento");
+            }
 	}
 	
 	/**
-	 * Mï¿½todo para efetuar uma consulta pelo nome da cidade informada
+	 * Método para efetuar uma consulta pelo nome da cidade informada
 	 * @param cidade_Nome
 	 * @return Cidade cd
 	 * @throws GeralException
@@ -74,21 +74,21 @@ public class NegocioCidade {
 	public Cidade consultar(String cidades_Nome) throws GeralException {
         Cidade cd = null;
         if( (cidades_Nome == null) || (cidades_Nome.equals("")) )
-        	throw new GeralException("Digite o nome ou o cï¿½dido da cidade!");
+        	throw new GeralException("Digite o nome ou o códido da cidade!");
         try{
         	cd = rep.consultar(cidades_Nome);
             if(cd == null)
-                throw new GeralException("Cidade nï¿½o estï¿½ cadastrada!");
+                throw new GeralException("Cidade não está cadastrada!");
         }catch(RepositorioException e){
-            throw new GeralException("Erro de programaï¿½ï¿½o!");
+            throw new GeralException("Erro de programação!");
         }catch(ConexaoException e){
-            throw new GeralException("O banco de dados nï¿½o estï¿½ acessï¿½vel no momento");
+            throw new GeralException("O banco de dados não está acessível no momento");
         }
         return cd;
     }
 	
 	/**
-	 * Mï¿½todo para efetuar uma consulta pelo cï¿½digo de uma cidade
+	 * Método para efetuar uma consulta pelo código de uma cidade
 	 * @param cidade_Codigo
 	 * @return Cidade cd
 	 * @throws GeralException
@@ -96,86 +96,85 @@ public class NegocioCidade {
 	public Cidade consultar(int cidades_Codigo) throws GeralException {
         Cidade cd = null;
         if (cidades_Codigo <= 0)
-            throw new GeralException("Digite um cï¿½dido vï¿½lido!");
+            throw new GeralException("Digite um códido válido!");
         try{
             cd = rep.consultar(cidades_Codigo);
             if(cd == null)
-                throw new GeralException("Cidade nï¿½o estï¿½ cadastrada!");
+                throw new GeralException("Cidade não está cadastrada!");
         }catch(RepositorioException e){
-            throw new GeralException("Erro de programaï¿½ï¿½o!");
+            throw new GeralException("Erro de programação!");
         }catch(ConexaoException e){
-            throw new GeralException("O banco de dados nï¿½o estï¿½ acessï¿½vel no momento");
+            throw new GeralException("O banco de dados não está acessível no momento");
         }
         return cd;
     }
 	
 	/**
-	 * Valida o cï¿½digo da cidade antes de efetuar a exclusï¿½o.
+	 * Valida o código da cidade antes de efetuar a exclusão.
 	 * @param cidade_Codigo
 	 * @return
 	 */
 	public void excluir(int cidades_Codigo) throws GeralException {
 		if (cidades_Codigo <= 0) {
-			throw new GeralException("Digite um cï¿½dido vï¿½lido!");
+			throw new GeralException("Digite um códido válido!");
 		}
 		try{			
 			Cidade cd = rep.consultar(cidades_Codigo);
 			if( cd == null ){
-				throw new GeralException("Cidade nï¿½o estï¿½ cadastrada!");
+				throw new GeralException("Cidade não está cadastrada!");
 			}
 			rep.excluir(cidades_Codigo);
 		} catch (RepositorioException ex){
-			throw new GeralException("Registro nï¿½o pode ser excluï¿½do!\nMotivo: Existem um ou mais endereï¿½os cadastrados para esta cidade.");
+			throw new GeralException("Registro não pode ser excluído!\nMotivo: Existem um ou mais endereços cadastrados para esta cidade.");
 		} catch (ConexaoException ex){
-			throw new GeralException("O banco de dados nï¿½o estï¿½ acessï¿½vel!");
+			throw new GeralException("O banco de dados não está acessível no momento");
 		}
 	}
 	
 	public void excluir(String cidades_Nome) throws GeralException {
 		if( (cidades_Nome == null) || (cidades_Nome.equals("")) ) {
-			throw new GeralException("Digite o nome ou o cï¿½dido da cidade!");
+			throw new GeralException("Digite o nome ou o códido da cidade!");
 		}
 		try{			
 			Cidade cd = rep.consultar(cidades_Nome);
 			if( cd == null ){
-				throw new GeralException("Cidade nï¿½o estï¿½ cadastrada!");
+				throw new GeralException("Cidade não está cadastrada!");
 			}
 			
 			rep.excluir(cidades_Nome);
 			
-		}catch (RepositorioException ex){
-			throw new GeralException("Registro nï¿½o pode ser excluï¿½do!\nMotivo: Existem um ou mais endereï¿½os cadastrados para esta cidade.");
-			
-		}catch (ConexaoException ex){
-			throw new GeralException("O banco de dados nï¿½o estï¿½ acessï¿½vel!");
+		} catch (RepositorioException ex){
+			throw new GeralException("Registro não pode ser excluído!\nMotivo: Existem um ou mais endereços cadastrados para esta cidade.");
+		} catch (ConexaoException ex){
+			throw new GeralException("O banco de dados não está acessível no momento");
 		}
 	}
 	
 	/**
-	 * Mï¿½todo para alterar um registro a partir de seu cï¿½digo informado.
+	 * Método para alterar um registro a partir de seu código informado.
 	 * @param cd Cidade
 	 * @throws GeralException
 	 */
 	public void alterar(Cidade cd) throws GeralException {
 		
 		if( cd.getCidades_Codigo() <= 0 )
-            throw new GeralException("Digite um cï¿½dido vï¿½lido!");
+            throw new GeralException("Digite um códido válido!");
 		
 		if ( (cd.getCidades_Nome() == null) || (cd.getCidades_Nome().equals("")) )
-			throw new GeralException("Digite o nome ou o cï¿½dido da cidade!");
+			throw new GeralException("Digite o nome ou o códido da cidade!");
 			
         try{
             Cidade cdConsul = rep.consultar(cd.getCidades_Codigo());
             if( cdConsul == null ){
-				throw new GeralException("Cidade nï¿½o estï¿½ cadastrada!");
+				throw new GeralException("Cidade não está cadastrada!");
             }
 			
            rep.alterar(cd);
             
         } catch (RepositorioException ex){
-			throw new GeralException("Erro de programaï¿½ï¿½o!");
+			throw new GeralException("Erro de programação!");
 		} catch (ConexaoException ex){
-			throw new  GeralException("O banco de dados nï¿½o estï¿½ acessï¿½vel!");
+			throw new GeralException("O banco de dados não está acessível no momento");
 		}
 	}
 	
@@ -184,15 +183,15 @@ public class NegocioCidade {
 		ArrayList<Cidade> lista;
 		
 		if (cidades_Nome == null) {
-        	throw new GeralException("Digite o nome ou o cï¿½dido da cidade!");
+        	throw new GeralException("Digite o nome ou o códido da cidade!");
 		}
 		try{
 			lista = (ArrayList<Cidade>)new RepositorioCidade().listar(cidades_Nome);
 		
 		}catch (RepositorioException ex ){
-			throw new GeralException("Erro de programaï¿½ï¿½o!");
+			throw new GeralException("Erro de programação!");
 		}catch (ConexaoException ex ){
-			throw new GeralException("O banco de dados nï¿½o estï¿½ acessï¿½vel!");
+			throw new GeralException("O banco de dados não está acessível no momento");
 		}return lista;
 	}
 }
