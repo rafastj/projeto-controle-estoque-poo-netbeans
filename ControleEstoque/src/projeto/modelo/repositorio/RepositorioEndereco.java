e52db5f8e4b87ef3f579bd3131d16740b64123a9
danielvdantas <danielvdantas@hotmail.com>
2012-06-02 17:48:49 +0000

    Substituindo meus formulários JFrame por JDialog.

--- ControleEstoque/src/projeto/modelo/repositorio/RepositorioEndereco.java
+++ ControleEstoque/src/projeto/modelo/repositorio/RepositorioEndereco.java
@@ -158,10 +158,10 @@ public class RepositorioEndereco implements IRepositorioEndereco {
         ArrayList<Endereco> lista = new ArrayList<Endereco>();
         Endereco end;
         Connection c = g.conectar();
-        String sqlLista = "SELECT end.enderecos_Codigo, end.enderecos_CEP, end.enderecos_Logradouro, cd.cidades_Codigo, cd.cidades_Nome from Enderecos as end inner join Cidades as cd On end.cidades_Codigo = cd.cidades_Codigo where end.Enderecos_CEP = ? ORDER BY end.enderecos_CEP";
+        String sqlLista = "SELECT end.enderecos_Codigo, end.enderecos_CEP, end.enderecos_Logradouro, cd.cidades_Codigo, cd.cidades_Nome from Enderecos as end inner join Cidades as cd On end.cidades_Codigo = cd.cidades_Codigo where end.Enderecos_CEP LIKE ? ORDER BY end.enderecos_CEP";
         try {
             PreparedStatement pstm = c.prepareStatement(sqlLista);
-            pstm.setString(1, enderecos_CEP);
+            pstm.setString(1, enderecos_CEP+"%");
             ResultSet rs = pstm.executeQuery();
             //verifica se retornou algum registro e cria os Objetos
             while (rs.next()) {
