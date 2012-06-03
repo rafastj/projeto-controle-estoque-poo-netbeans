99d6afc6cd217af82e4a198baaf34dcc0ffbab36
diegodwfacul@gmail.com <diegodwfacul@gmail.com@8d6f3852-27d6-a5f3-d47a-d68ca35f34d9>
2012-06-03 20:05:34 +0000

    Componente jTextField que só aceita Ponto,Vircula e Numeros de 0 a 9.

---
+++ trunk/ControleEstoque/src/projeto/modelo/componente/ValorReal.java
@@ -0,0 +1,54 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projeto.modelo.componente;
+
+import javax.swing.text.AttributeSet;
+import javax.swing.JTextField;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Document;
+import javax.swing.text.PlainDocument;
+
+/**
+ *
+ * @author diego
+ */
+public class ValorReal extends JTextField{
+    
+    private static final long serialVersionUID = 1L;
+    
+    public ValorReal(){
+        super();
+    }
+    
+    public ValorReal(int cols){
+        super(cols);
+    }
+    
+    @Override
+     protected Document createDefaultModel() {  
+          return (Document) new NumberDocument();  
+     }  
+  
+     static class NumberDocument extends PlainDocument {  
+  
+        private static final long serialVersionUID = 1L;  
+  
+        @Override
+        public void insertString(int offs, String str, AttributeSet a) throws BadLocationException {  
+              if (str == null) return;  
+
+              for (int i = 0; i < str.length(); i++) {
+                  if(str.equals(",") || str.equals(".")){ //aqui ele irar permitir ponto e virgula
+                      
+                  }else if(Character.isDigit(str.charAt(i))== false){ //aqui ele verificar se for numero 
+                      System.out.println(str.charAt(i));
+                      return;  
+                  }  
+              }  
+              super.insertString(offs, str, a);  
+          }  
+    
+    }
+}
